b52625d6672d86e45c6e6a5f0fcf35c487d36d31
Christa Hartsock <christa@codeforamerica.org>
2018-09-17 16:53:49 -0700

    Throw helpful error if master.key is missing
    Only on development + production environments, since we shouldn't require master key for tests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Throw helpful error if master.key is missing
+  config.require_master_key = true
 end
