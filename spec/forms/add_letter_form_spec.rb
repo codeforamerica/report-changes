fe6824b4a0ad9078c14ad348ccb91b19f47a4256
Whitney Schaefer <whitney@codeforamerica.org>
2018-09-25 10:02:23 -0700

    Remove letter validation
    [#160337075]

--- spec/forms/add_letter_form_spec.rb
+++ spec/forms/add_letter_form_spec.rb
@@ -1,40 +1,6 @@
 require "rails_helper"
 
 RSpec.describe AddLetterForm do
-  describe "validations" do
-    context "when letters is provided" do
-      it "is valid" do
-        form = AddLetterForm.new(
-          letters: ["best attribute"],
-        )
-
-        expect(form).to be_valid
-      end
-    end
-
-    context "when letters is just the empty string that comes from the client" do
-      it "is invalid" do
-        form = AddLetterForm.new(
-          letters: [""],
-        )
-
-        expect(form).not_to be_valid
-        expect(form.errors[:letters]).to be_present
-      end
-    end
-
-    context "when letters is not provided" do
-      it "is invalid" do
-        form = AddLetterForm.new(
-          letters: nil,
-        )
-
-        expect(form).not_to be_valid
-        expect(form.errors[:letters]).to be_present
-      end
-    end
-  end
-
   describe "#save" do
     let(:change_report) { create :change_report }
     let(:file) { fixture_file_upload(Rails.root.join("spec", "fixtures", "image.jpg"), "image/jpg") }
