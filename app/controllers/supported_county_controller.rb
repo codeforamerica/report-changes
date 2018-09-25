25bf3e3cb12bad8f4b9b7ac9346504ca40ac7eb9
Whitney Schaefer <whitney@codeforamerica.org>
2018-09-25 14:50:55 -0700

    Fix broken tests

--- app/controllers/supported_county_controller.rb
+++ app/controllers/supported_county_controller.rb
@@ -1,6 +1,6 @@
 class SupportedCountyController < FormsController
   def self.show_rule_sets(change_report)
-    super << change_report.navigator.county_from_address == "Arapahoe County"
+    super << (change_report.navigator.county_from_address == "Arapahoe County")
   end
 
   def form_class
