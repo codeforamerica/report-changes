93b0d696129c9a06fe1e32840bcef35fa1706e33
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-09-25 13:25:36 -0700

    Letters can be deleted
    [#160337075]
    
    Signed-off-by: Whitney Schaefer <whitney@codeforamerica.org>
    Co-authored-by: Whitney Schaefer <whitney@codeforamerica.org>

--- spec/forms/add_letter_form_spec.rb
+++ spec/forms/add_letter_form_spec.rb
@@ -3,12 +3,18 @@ require "rails_helper"
 RSpec.describe AddLetterForm do
   describe "#save" do
     let(:change_report) { create :change_report }
-    let(:file) { fixture_file_upload(Rails.root.join("spec", "fixtures", "image.jpg"), "image/jpg") }
+    let(:active_storage_blob) do
+      ActiveStorage::Blob.create_after_upload!(
+        io: File.open(Rails.root.join("spec", "fixtures", "image.jpg")),
+        filename: "image.jpg",
+        content_type: "image/jpg",
+      )
+    end
 
     let(:valid_params) do
       {
         change_report: change_report,
-        letters: ["", file],
+        letters: ["", active_storage_blob.signed_id],
       }
     end
 
@@ -23,7 +29,7 @@ RSpec.describe AddLetterForm do
     end
 
     it "ignores values that are already present" do
-      change_report.letters.attach(file)
+      change_report.letters.attach(active_storage_blob)
       valid_params[:letters] = [change_report.letters.first.signed_id]
 
       form = AddLetterForm.new(valid_params)
@@ -32,6 +38,18 @@ RSpec.describe AddLetterForm do
       expect do
         form.save
       end.to_not(change { change_report.letters.count })
+    end
+
+    it "removes values that are not included anymore" do
+      change_report.letters.attach(active_storage_blob)
+      valid_params[:letters] = []
+
+      form = AddLetterForm.new(valid_params)
+      form.valid?
+
+      expect do
+        form.save
+      end.to(change { change_report.letters.count }.from(1).to(0))
     end
   end
 end
