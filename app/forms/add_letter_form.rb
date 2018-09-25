5ef77315e7043447b88e88c03b85119149768091
Whitney Schaefer <whitney@codeforamerica.org>
2018-09-25 10:02:23 -0700

    Remove letter validation
    [#160337075]

--- app/forms/add_letter_form.rb
+++ app/forms/add_letter_form.rb
@@ -1,19 +1,11 @@
 class AddLetterForm < Form
   set_attributes_for :change_report, letters: []
 
-  before_validation :no_blanks
-  validates_presence_of :letters, message: "Please attach your letter."
-
   def save
     letters_to_attach = letters.reject do |letter_signed_id|
-      change_report.letters.map(&:signed_id).include?(letter_signed_id)
+      letter_signed_id.blank? ||
+        change_report.letters.map(&:signed_id).include?(letter_signed_id)
     end
     change_report.letters.attach(letters_to_attach)
-  end
-
-  private
-
-  def no_blanks
-    self.letters = letters.reject(&:blank?) if letters.present?
   end
 end
--- spec/controllers/add_letter_controller_spec.rb
+++ spec/controllers/add_letter_controller_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe AddLetterController do
   it_behaves_like "form controller base behavior"
-  it_behaves_like "form controller unsuccessful update"
 
   describe "#update" do
     context "without change report" do
