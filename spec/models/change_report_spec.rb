f0ad89e1aea1ca769258a5284b1aa7c70ac91159
Whitney Schaefer <whitney@codeforamerica.org>
2018-10-02 15:07:06 -0700

    Attach pdfs and images to generated change report
    images are rendered in the html and converted using wicked_pdf the same way the rest of the change report is generated
    
    pdfs are attached using CombinePdf
    
    [#160521773]

--- spec/models/change_report_spec.rb
+++ spec/models/change_report_spec.rb
@@ -38,4 +38,23 @@ RSpec.describe ChangeReport, type: :model do
       )
     end
   end
+
+  describe "#pdf_letters" do
+    it "returns all letters that are content_type application/pdf" do
+      change_report = create :change_report
+      change_report.letters.attach(
+        io: File.open(Rails.root.join("spec", "fixtures", "image.jpg")),
+        filename: "image.jpg",
+        content_type: "image/jpg",
+      )
+      change_report.letters.attach(
+        io: File.open(Rails.root.join("spec", "fixtures", "document.pdf")),
+        filename: "document.pdf",
+        content_type: "application/pdf",
+      )
+
+      expect(change_report.pdf_letters.count).to eq 1
+      expect(change_report.pdf_letters.first.filename).to eq "document.pdf"
+    end
+  end
 end
