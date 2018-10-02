7459e9385cc0a6d9fea92b1b3192d42e1b7b305d
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-10-02 14:13:37 -0400

    Test mixpanel events in each controller spec instead of the full feature spec
    Removes our setting of Capybara's max wait time
    
    [#160402748]
    
    Co-Authored-By: Andrew Hyder <hyd415@gmail.com>
    Co-Authored-By: Christa Hartsock <christa.hartsock@gmail.com>

--- spec/support/shared_examples/forms_controller_successful_update.rb
+++ spec/support/shared_examples/forms_controller_successful_update.rb
@@ -23,6 +23,21 @@ RSpec.shared_examples_for "form controller successful update" do |valid_params|
 
           expect(response).to redirect_to(subject.next_path)
         end
+
+        let(:mock_mixpanel_service) { spy(MixpanelService) }
+
+        it "calls the mixpanel service" do
+          allow(MixpanelService).to receive(:instance).and_return(mock_mixpanel_service)
+
+          put :update, params: { form: valid_params }
+
+          current_change_report.reload
+          expect(mock_mixpanel_service).to have_received(:run).with(
+            unique_id: current_change_report.id,
+            event_name: controller.form_class.analytics_event_name,
+            data: current_change_report.mixpanel_data,
+          )
+        end
       end
     end
   end
