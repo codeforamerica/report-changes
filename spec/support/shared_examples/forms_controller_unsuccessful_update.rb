f84d7aa967550f2fd8738a2418ef65bba26a4734
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-10-02 17:11:16 -0400

    Send mixpanel events on validation errors
    [#160402748]
    
    Co-Authored-By: Andrew Hyder <hyd415@gmail.com>

--- spec/support/shared_examples/forms_controller_unsuccessful_update.rb
+++ spec/support/shared_examples/forms_controller_unsuccessful_update.rb
@@ -3,12 +3,30 @@ require "rails_helper"
 RSpec.shared_examples_for "form controller unsuccessful update" do |invalid_params|
   describe "#update" do
     context "on unsucessful update" do
-      it "rerenders edit template" do
-        current_change_report = create(:change_report, :with_navigator)
+      let(:mock_mixpanel_service) { spy(MixpanelService) }
+      let(:current_change_report) { create(:change_report, :with_navigator) }
+
+      before do
         session[:current_change_report_id] = current_change_report.id
+        allow(MixpanelService).to receive(:instance).and_return(mock_mixpanel_service)
 
         put :update, params: { form: invalid_params || {} }
+      end
 
+      it "sends a mixpanel event with the validation error" do
+        data = {
+          screen: controller.form_class.analytics_event_name,
+          errors: assigns(:form).errors.messages.keys,
+        }.merge(current_change_report.mixpanel_data)
+
+        expect(mock_mixpanel_service).to have_received(:run).with(
+          unique_id: current_change_report.id,
+          event_name: "validation_error",
+          data: data,
+        )
+      end
+
+      it "rerenders edit template" do
         expect(response).to render_template(:edit)
       end
     end
