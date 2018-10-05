362b41a5db7be2f87a9e3eeaa640529b65cf2055
Whitney Schaefer <whitney@codeforamerica.org>
2018-10-05 13:00:10 -0700

    refactor code relying on ActiveStorage::Service private method path_for
    - that method works locally but is undefined on heroku
    - the alternative downloads the file and returns the path
    
    [#160521773]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,11 @@ module ApplicationHelper
     "(#{phone[0..2]}) #{phone[3..5]}-#{phone[6..9]}" if phone
   end
 
-  def wicked_blob_path(file)
-    ActiveStorage::Blob.service.send(:path_for, file.blob.key)
+  def wicked_blob_path(image)
+    save_path = Rails.root.join("tmp", "#{image.id}_#{image.filename}")
+    File.open(save_path, "wb") do |file|
+      file << image.blob.download
+    end
+    save_path.to_s
   end
 end
