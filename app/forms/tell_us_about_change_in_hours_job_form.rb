796ea32875a0ac145f4caef6ee5d5d12b2971780
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-11-06 13:25:15 -0800

    Ask client about job for change in hours flow
    [Finishes #161729765]
    
    Signed-off-by: Christa Hartsock <christa@codeforamerica.org>

---
+++ app/forms/tell_us_about_change_in_hours_job_form.rb
@@ -0,0 +1,14 @@
+class TellUsAboutChangeInHoursJobForm < Form
+  set_attributes_for :change_report,
+                     :company_name, :manager_name, :manager_phone_number
+
+  before_validation -> { strip_dashes(:manager_phone_number) }
+
+  validates_presence_of :company_name, message: "Please add a name."
+  validates_presence_of :manager_name, message: "Please add a name."
+  validates :manager_phone_number, ten_digit_phone_number: true
+
+  def save
+    change_report.update(attributes_for(:change_report))
+  end
+end
