fce960a624acf9a1b338fa00dd84e25978a0da25
Christa Hartsock <christa@codeforamerica.org>
2018-11-15 14:40:40 -0800

    Default sort by created_at for administrate 0.11.0
    Administrate 0.11.0 now expects ordering params to be nested under the
    resource name rather than at root.
    
    [Fixes #161830921]

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,8 +10,10 @@ module Admin
     around_action :add_tags_to_logs
 
     def default_params
-      params[:order] ||= "created_at"
-      params[:direction] ||= "desc"
+      resource_params = params.fetch(resource_name, {})
+      order = resource_params.fetch(:order, "created_at")
+      direction = resource_params.fetch(:direction, "desc")
+      params[resource_name] = resource_params.merge(order: order, direction: direction)
     end
 
     def add_tags_to_logs
