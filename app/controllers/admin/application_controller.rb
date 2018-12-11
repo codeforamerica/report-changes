11b979dbd92180166282e125aae4c7a5079c1f0b
Andrew Hyder <hyd415@gmail.com>
2018-12-10 16:52:59 -0800

    Log reports ids in admin views

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,18 +7,12 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :default_params
-    around_action :add_tags_to_logs
 
     def default_params
       resource_params = params.fetch(resource_name, {})
       order = resource_params.fetch(:order, "created_at")
       direction = resource_params.fetch(:direction, "desc")
       params[resource_name] = resource_params.merge(order: order, direction: direction)
-    end
-
-    def add_tags_to_logs
-      user_email = current_admin_user.email
-      Rails.logger.tagged(user_email) { yield }
     end
   end
 end
--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,8 +3,19 @@ Rails.application.configure do
 
   config.lograge.custom_payload do |controller|
     {
-      report_id: controller.try(:current_report).try(:id),
+      report_id: report_id(controller),
       admin_user_email: controller.try(:current_admin_user).try(:email),
     }
+  end
+
+  def report_id(controller)
+    params = controller.params
+    if params[:controller] == "admin/reports" && params[:id]
+      params[:id]
+    elsif params[:controller] ==  "admin/changes" && params[:id]
+      Change.find(params[:id]).report.id
+    else
+      controller.try(:current_report).try(:id)
+    end
   end
 end
