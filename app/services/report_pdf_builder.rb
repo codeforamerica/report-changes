75d462e905fbb1aca595d75e8945e09d6fa0888f
Andrew Hyder <hyd415@gmail.com>
2019-01-07 16:23:40 -0800

    Documents are now organized by reported change in the report pdf

--- app/services/report_pdf_builder.rb
+++ app/services/report_pdf_builder.rb
@@ -1,22 +1,36 @@
 class ReportPdfBuilder
-  attr_reader :pdf_from_html, :attachments
+  attr_reader :report_pdf_from_html, :report
 
   def initialize(report)
-    rendered_string = ApplicationController.render(
+    @report = report
+
+    report_pdf_string = ApplicationController.render(
       layout: "report",
       template: "reports/change_report",
       assigns: { report: report },
     )
-    @pdf_from_html = WickedPdf.new.pdf_from_string(rendered_string)
-
-    @attachments = report.pdf_documents
+    @report_pdf_from_html = WickedPdf.new.pdf_from_string(report_pdf_string)
   end
 
   def run
-    combined_file = CombinePDF.parse(pdf_from_html)
-    attachments.each do |attachment|
-      combined_file << CombinePDF.parse(attachment.download)
+    combined_file = CombinePDF.parse(report_pdf_from_html)
+
+    report.reported_changes.each do |change|
+      # cover page and images
+      change_html = ApplicationController.render(
+        layout: "change_docs",
+        template: "reports/change_docs",
+        assigns: { change: change },
+      )
+      change_docs_pdf_from_html = WickedPdf.new.pdf_from_string(change_html)
+      combined_file << CombinePDF.parse(change_docs_pdf_from_html)
+
+      # pdfs
+      change.pdf_documents.each do |pdf|
+        combined_file << CombinePDF.parse(pdf.download)
+      end
     end
+
     combined_file.to_pdf
   end
 end
