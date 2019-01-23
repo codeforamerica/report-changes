0b4438ae28122afcf591a188959e9833aaba9768
Andrew Hyder <hyd415@gmail.com>
2019-01-23 12:20:41 -0800

    Locked out after three attempts.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,4 @@
 class AdminUser < ApplicationRecord
   devise :otp_authenticatable, :database_authenticatable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :lockable
 end
