615f919c1f5821b2f9f327313734ac3796232c21
Andrew Hyder <andrewh@codeforamerica.org>
2019-01-23 15:41:29 -0800

    User accounts are set as inactive after 90 days of no activity.
    Admins can reset them by setting last_sign_in_at to nil
    
    Signed-off-by: Whitney Schaefer <whitney@codeforamerica.org>

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,12 @@ class AdminUser < ApplicationRecord
   devise :otp_authenticatable, :database_authenticatable,
          :recoverable, :rememberable, :validatable, :trackable,
          :timeoutable, :lockable
+
+  def active_for_authentication?
+    super && (last_sign_in_at.nil? || last_sign_in_at > 90.days.ago)
+  end
+
+  def inactive_message
+    "It's been over 90 days since you last logged in, contact an admin to regain access."
+  end
 end
