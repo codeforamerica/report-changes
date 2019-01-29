1b25754c3de7f97ac5ce6b63405c732b6d52c6a1
Andrew Hyder <hyd415@gmail.com>
2019-01-29 10:51:31 -0800

    Small improvements from code review

--- spec/controllers/do_you_have_lost_job_documents_controller_spec.rb
+++ spec/controllers/do_you_have_lost_job_documents_controller_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe DoYouHaveLostJobDocumentsController do
   it_behaves_like "form controller job termination behavior"
-  it_behaves_like "form controller successful update", { has_job_termination_documents: "yes" } # , "job_termination"
+  it_behaves_like "form controller successful update", { has_job_termination_documents: "yes" }, "job_termination"
   it_behaves_like "form controller unsuccessful update"
 end
