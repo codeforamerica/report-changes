1e52c08a47c87a0d9646e80f22102bb5322c1844
Andrew Hyder <hyd415@gmail.com>
2019-01-29 16:12:02 -0800

    Find the most recently created change for this report

--- app/controllers/change_type_controller.rb
+++ app/controllers/change_type_controller.rb
@@ -1,7 +1,1 @@
-class ChangeTypeController < FormsController
-  def next_path
-    if params[:form][:change_type] == "job_termination"
-      screen_path(TellUsAboutTheLostJobController.to_param)
-    end
-  end
-end
+class ChangeTypeController < FormsController; end
