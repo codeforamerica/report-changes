8889d1eda7cb76023181d59e3691c38ebf2c721d
Andrew Hyder <hyd415@gmail.com>
2019-02-20 16:00:42 -0800

    Improvements from code review.

--- spec/support/shared_examples/forms_controller_change_in_hours_behavior.rb
+++ spec/support/shared_examples/forms_controller_change_in_hours_behavior.rb
@@ -4,22 +4,9 @@ RSpec.shared_examples_for "form controller change in hours behavior" do
   it_behaves_like "form controller base behavior", "change_in_hours"
 
   describe "show?" do
-    let(:report) { create :report, :filled }
-
-    before do
-      member = create :member, report: report
-      change = create :change,
-        change_type: change_type,
-        member: member
-      create :navigator,
-        current_member: member,
-        current_change: change,
-        report: report
-    end
+    let(:report) { create :report, :filled, change_type: "change_in_hours" }
 
     context "when change_type is change_in_hours" do
-      let(:change_type) { "change_in_hours" }
-
       it "returns true" do
         show_form = subject.class.show?(report)
         expect(show_form).to eq(true)
@@ -27,9 +14,9 @@ RSpec.shared_examples_for "form controller change in hours behavior" do
     end
 
     context "when change_type is new_job" do
-      let(:change_type) { "new_job" }
-
       it "returns false" do
+        report.current_change.update change_type: "new_job"
+
         show_form = subject.class.show?(report)
         expect(show_form).to eq(false)
       end
--- spec/support/shared_examples/forms_controller_job_termination_behavior.rb
+++ spec/support/shared_examples/forms_controller_job_termination_behavior.rb
@@ -4,12 +4,10 @@ RSpec.shared_examples_for "form controller job termination behavior" do
   it_behaves_like "form controller base behavior", "job_termination"
 
   describe "show?" do
-    let(:report) { create :report, :filled }
+    let(:report) { create :report, :filled, change_type: "job_termination" }
 
     context "when change_type is job_termination" do
       it "returns true" do
-        report.current_change.update change_type: "job_termination"
-
         show_form = subject.class.show?(report)
 
         expect(show_form).to eq(true)
