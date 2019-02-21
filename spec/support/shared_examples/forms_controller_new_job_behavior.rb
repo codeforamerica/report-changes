70c41648f3d14256fc7499f3ad9ba1e5386a5a19
Andrew Hyder <hyd415@gmail.com>
2019-02-20 16:00:42 -0800

    Improvements from code review.

--- spec/support/shared_examples/forms_controller_new_job_behavior.rb
+++ spec/support/shared_examples/forms_controller_new_job_behavior.rb
@@ -4,13 +4,10 @@ RSpec.shared_examples_for "form controller new job behavior" do
   it_behaves_like "form controller base behavior", "new_job"
 
   describe "show?" do
+    let(:report) { create :report, :filled, change_type: "new_job" }
+
     context "when change_type is new job" do
       it "returns true" do
-        report = create :report
-        member = create :member, report: report
-        change = create :change, member: member, change_type: "new_job"
-        create :navigator, current_member: member, current_change: change, report: report
-
         show_form = subject.class.show?(report)
         expect(show_form).to eq(true)
       end
@@ -18,10 +15,7 @@ RSpec.shared_examples_for "form controller new job behavior" do
 
     context "when change_type is not new_job" do
       it "returns false" do
-        report = create :report
-        member = create :member, report: report
-        change = create :change, member: member, change_type: "job_termination"
-        create :navigator, current_member: member, current_change: change, report: report
+        report.current_change.update change_type: "change_in_hours"
 
         show_form = subject.class.show?(report)
         expect(show_form).to eq(false)
