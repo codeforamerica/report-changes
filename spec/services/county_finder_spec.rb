2fb9b9bd48b0cf1d2e26a157ae924dd494a6301a
Andrew Hyder <hyd415@gmail.com>
2019-03-14 10:31:28 -0700

    Add Pitkin county

--- spec/services/county_finder_spec.rb
+++ spec/services/county_finder_spec.rb
@@ -2,8 +2,12 @@ require "rails_helper"
 
 RSpec.describe CountyFinder do
   describe ".from_zip_code" do
-    it "should find the county based on zip_code" do
+    it "should find Araphaoe county based on zip_code" do
       expect(described_class.from_zip_code("80046")).to eq "Arapahoe"
+    end
+
+    it "should find Pitkin county based on zip_code" do
+      expect(described_class.from_zip_code("81611")).to eq "Pitkin"
     end
 
     it "should return nil if no results can be found" do
