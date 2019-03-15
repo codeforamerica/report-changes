ed6420b658926c7fd3184f0f102e875d27ce2b00
Andrew Hyder <andrewh@codeforamerica.org>
2019-03-15 11:56:04 -0700

    Move county emails from credentials to environment.
    Signed-off-by: Luigi Ray-Montanez <luigi@codeforamerica.org>

--- spec/services/county_finder_spec.rb
+++ spec/services/county_finder_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe CountyFinder do
       expect(described_class.from_zip_code("00000")).to be_nil
     end
   end
+
+  describe ".email_address" do
+    it "should return the email of the report's county" do
+      with_modified_env ARAPAHOE_EMAIL_ADDRESS: "arapahoe@email.com" do
+        report = create(:report, county: "Arapahoe")
+        expect(described_class.email_address(report)).to eq "arapahoe@email.com"
+      end
+    end
+
+    it "should return nil when the county is nil" do
+      report = create(:report, county: nil)
+      expect(described_class.email_address(report)).to be_nil
+    end
+  end
 end
