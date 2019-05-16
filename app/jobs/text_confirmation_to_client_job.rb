0e69815e2e80e28dc7b5d984155c0ab693f8a384
Andrew Hyder <hyd415@gmail.com>
2019-05-16 12:09:08 -0700

    Move twilio creds to env out of credentials file

--- app/jobs/text_confirmation_to_client_job.rb
+++ app/jobs/text_confirmation_to_client_job.rb
@@ -1,8 +1,8 @@
 class TextConfirmationToClientJob < ApplicationJob
   def perform(phone_number:)
-    Twilio::REST::Client.new.studio.flows("FW00aff42f48d3eb6a6e4c5375c716d588").executions.create(
+    Twilio::REST::Client.new.studio.flows(ENV["TWILIO_STUDIO_FLOW"]).executions.create(
       to: phone_number,
-      from: "+12564084607",
+      from: ENV["TWILIO_PHONE_NUMBER"],
     )
   end
 end
